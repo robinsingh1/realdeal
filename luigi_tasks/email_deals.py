6396be4a1d6070c822cb71890cece38f0006d784
Ben Pitzer <ben.pitzer@gmail.com>
2016-04-20 00:31:48 -0400

    Send html email

--- luigi_tasks/email_deals.py
+++ luigi_tasks/email_deals.py
@@ -19,6 +19,7 @@ PROPERTY_TABLE_FIELDS = [
   'building_size',
   'purchase_price',
   'zestimate_amount',
+  'zillow_url',
 ]
 
 PROPERTY_TABLE_FIELD_TYPES = [
@@ -29,10 +30,11 @@ PROPERTY_TABLE_FIELD_TYPES = [
   'float',
   'int',
   'dollar',
-  'dollar',                       
+  'dollar',       
+  'link',                 
 ]
 
-EMAIL_TO = "ben.pitzer@gmail.com"
+EMAIL_TO = "realdeal-bay-area@googlegroups.com"
 EMAIL_FROM = "ben.pitzer@gmail.com"                 
 
 locale.setlocale( locale.LC_ALL, '' )
@@ -43,16 +45,16 @@ class EmailDeals(RealDealBaseTask):
     return self.getLocalFileTarget("properties_emailed.json")
   
   def createPropertyTable(self, properties, fields, field_types): 
-    html = "<table class=\"tg\">\n"
-    
+    html = "<table style=\"border-collapse:collapse;border-spacing:0;border-color:#ccc\">\n"
+ 
     # Table headers
     html += "<tr>\n"
     for field, field_type in zip(fields, field_types):
       if field_type in ['int', 'float', 'dollar']:
-        css_class = 'tg-header-right'
+        text_align = 'right'
       else:
-        css_class = 'tg-header-left'
-      html += "<th class=\"%s\">%s</th>\n" % (css_class, field)
+        text_align = 'left'
+      html += "<th style=\"font-family:Arial, sans-serif;font-size:14px;font-weight:bold;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;background-color:#f0f0f0;vertical-align:top;text-align:%s\">%s</th>\n" % (text_align, field)
     html += "</tr>\n"       
     
     # Table data
@@ -61,22 +63,27 @@ class EmailDeals(RealDealBaseTask):
       for field, field_type in zip(fields, field_types):
         if field_type == 'string':
           value = prop.get(field, "")
-          css_class = 'tg-data-left'
+          text_align = 'left'
         elif field_type == 'int':
           value = int(prop.get(field, 0))
-          css_class = 'tg-data-right'
+          text_align = 'right'
         elif field_type == 'float':
           value = float(prop.get(field, 0.0))
-          css_class = 'tg-data-right'
+          text_align = 'right'
         elif field_type == 'dollar':
           value = float(prop.get(field, 0.0))
           if value:
             value = locale.currency(value, grouping=True)
-          css_class = 'tg-data-right'   
-   
+          text_align = 'right'   
+        elif field_type == 'link':
+          value = prop.get(field, "")
+          if value:
+            value = "<a href=\"%s\">link</a>" % value
+          text_align = 'left' 
+          
         if not value:
           value = ""
-        html += "<td class=\"%s\">%s</td>\n" % (css_class, value)
+        html += "<td style=\"font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;background-color:#fff;vertical-align:top;text-align:%s\">%s</td>\n" % (text_align, value)
       html += "</tr>\n"
     html += "</table>\n"
     
@@ -88,36 +95,18 @@ class EmailDeals(RealDealBaseTask):
     with self.input().open() as fin, self.output().open('w') as fout:
       properties = json.load(fin)
       
-      html = "<html>\n" 
-
-      html += "<head>\n"
-      html += """
-        <style type="text/css">
-          .tg  {border-collapse:collapse;border-spacing:0;border-color:#ccc;}
-          .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;background-color:#fff;}
-          .tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;background-color:#f0f0f0;}
-          .tg .tg-header-left{font-weight:bold;vertical-align:top;text-align:left}
-          .tg .tg-header-right{font-weight:bold;vertical-align:top;text-align:right}
-          .tg .tg-data-left{vertical-align:top;text-align:left}
-          .tg .tg-data-right{vertical-align:top;text-align:right}
-        </style>
-      """
-      html += "</head>\n"
-      
-      html += "<body>\n"
+      html = ""
       html += "<div>\n"
-      html += "%d new properties found:" % len(properties)
+      html += "Hello, I found the following properties for you:"
       html += "</div>\n"
       html += "<div>\n"
       html += self.createPropertyTable(properties, 
                                        PROPERTY_TABLE_FIELDS,
                                        PROPERTY_TABLE_FIELD_TYPES)
       html += "</div>\n"
-      html += "</body>\n"
-      html += "</html>\n"
       
       print html
-      subject = "%d new properties found" % len(properties)
+      subject = "Found %d new properties" % len(properties)
       client.send(EMAIL_TO, subject, html, EMAIL_FROM)
       
       json_str = "[%s]" % ",\n".join([json.dumps(p) for p in properties])
