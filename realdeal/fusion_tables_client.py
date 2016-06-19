90f7b57ba4f8c4002765377c86a03c060e191fb8
Ben Pitzer <ben.pitzer@gmail.com>
2016-06-19 16:37:51 -0400

    Remove pdb statement

--- realdeal/fusion_tables_client.py
+++ realdeal/fusion_tables_client.py
@@ -146,7 +146,6 @@ class FusionTablesClient(object):
       sql += ", ".join(map(self.dictValuePad, row.values()))
       sql += ");"
     logging.info("INSERT ROWS: %s", sql)
-    import pdb; pdb.set_trace()
     response = self.executeWriteQuery(sql, multiplier=REQUESTS_PER_WRITE*len(rows))
     return self.flattenRowColumnData(response)
   
